3c39ad909f7fa8dda36fc4c16be0ca570e62be90
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2022-02-26 18:45:37 -0300

    criação layout login + estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,9 @@ import { UiPageFooterComponent } from './components/ui-page-footer/ui-page-foote
 import { UiPageHeaderComponent } from './components/ui-page-header/ui-page-header.component';
 import { UiPageLoginComponent } from './components/ui-page-login/ui-page-login.component';
 import { UiPageMainComponent } from './components/ui-page-main/ui-page-main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,10 @@ import { UiPageMainComponent } from './components/ui-page-main/ui-page-main.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/ui-page-login/ui-page-login.component.ts
+++ src/app/components/ui-page-login/ui-page-login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ui-page-login',
   templateUrl: './ui-page-login.component.html',
-  styleUrls: ['./ui-page-login.component.sass']
+  styleUrls: ['./ui-page-login.component.scss'],
 })
 export class UiPageLoginComponent implements OnInit {
+  login: FormGroup;
+  hide = true;
+  circuit: string = '../../assets/circuit.mp4'
+  constructor() {
+    this.login = new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
 
-  constructor() { }
+    });
+
+
+  }
+
+  submit(){
+    console.log(this.login.controls['username'].value);
+    console.log(this.login.controls['password'].value);
+    }
+
 
   ngOnInit() {
+
   }
 
 }
