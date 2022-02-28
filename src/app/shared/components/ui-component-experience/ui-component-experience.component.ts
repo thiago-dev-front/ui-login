14a80cad3777de3b6f6624a374b4ac25b6f510ac
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2022-02-28 16:00:09 -0300

    criação ui experience

--- src/app/shared/components/ui-component-experience/ui-component-experience.component.ts
+++ src/app/shared/components/ui-component-experience/ui-component-experience.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui-component-experience',
   templateUrl: './ui-component-experience.component.html',
-  styleUrls: ['./ui-component-experience.component.sass']
+  styleUrls: ['./ui-component-experience.component.scss']
 })
 export class UiComponentExperienceComponent implements OnInit {
 
