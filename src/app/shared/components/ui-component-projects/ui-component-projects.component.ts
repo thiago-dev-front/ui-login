ccb49f3a6f4cbcf54a69fd821095a8c252e07ce3
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2022-03-13 17:26:56 -0300

    responsive projects

--- src/app/shared/components/ui-component-projects/ui-component-projects.component.ts
+++ src/app/shared/components/ui-component-projects/ui-component-projects.component.ts
@@ -10,6 +10,7 @@ export class UiComponentProjectsComponent implements OnInit {
   dotz: string = '../../assets/ganhe-port.mp4';
 
   textNetshoes = `Trabalhei em conjunto com o time de desenvolvimento e operações para a construção do ecommerce Netshoes e Zattini, ambos utilizando ATG e Endeca (Oracle) e posteriormente migrando para o Freedom.`;
+  textDotz = `Atualmente trabalho no time de desenvolvimento Dotz e juntos construímos uma nova interface utilizando o Angular, esse trabalho foi um dos maiores desafios da minha carreira, mas também muito gratificante.`;
   constructor() { }
 
   ngOnInit() {
